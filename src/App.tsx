c073d9e693b938bee0f3c512d76516ca80824629
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-08 19:54:25 +0000

    write routes for main pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,41 @@
 import { Route, Routes } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import MealSearch from "./Pages/MealSearch";
+import { Link } from "react-router-dom";
+import "./App.css";
+import { useState } from "react";
+import Reviews from "./Pages/Reviews";
 
 function App(): JSX.Element {
+  const [signedInUserID, setSignedInUserID] = useState<number | undefined>();
   return (
-    <Routes>
-      <Route path="/" element={<Homepage />}></Route>
-      <Route path="/meal-search" element={<MealSearch />}></Route>
-    </Routes>
+    <>
+      <h1>chefbook</h1>
+      <nav>
+        {!signedInUserID && <button>sign in</button>}
+        <Link to="/reviews">| reviews</Link>
+        <Link to="/meal-search"> | meal search</Link>
+        {signedInUserID && <button>sign out</button>}
+      </nav>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/meal-search" element={<MealSearch />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route
+          path="/meal-search/categories/:category"
+          element={<h1>category</h1>}
+        />
+        <Route
+          path="/meal-search/main-ingredient/:ingredient"
+          element={<h1>ingredient</h1>}
+        />
+        <Route
+          path="/meal-search/nationality/:country"
+          element={<h1>nationality</h1>}
+        />
+        <Route path="/meal-search/random" element={<h1>random</h1>} />
+      </Routes>
+    </>
   );
 }
 
