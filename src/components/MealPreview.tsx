499b304d1f8630615cab976e7e945cbf3e5331a7
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-10 20:50:17 +0000

    mealPreview update prop type

--- src/components/MealPreview.tsx
+++ src/components/MealPreview.tsx
@@ -1,14 +1,16 @@
+import Meal from "../utils/interfaces/IMeal";
 import IMealByIngredientOrNationOrCategory from "../utils/interfaces/IMealByIngredientOrNationOrCat";
 
 interface IMealPreview {
-  meal: IMealByIngredientOrNationOrCategory;
+  meal: IMealByIngredientOrNationOrCategory | Meal;
 }
 
-export default function MealPreview({ meal }: IMealPreview): JSX.Element {
+export default function MealPreviewA(props: IMealPreview): JSX.Element {
+  const meal = props.meal;
   return (
     <>
-      {meal.strMealThumb && <img src={meal.strMealThumb} alt="" />}
       <h1>{meal.strMeal}</h1>
+      {meal.strMealThumb !== null && <img src={meal.strMealThumb} alt="" />}
     </>
   );
 }
