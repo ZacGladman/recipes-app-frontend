1d3f283538c98f24a168d71b42e7b9ddadb7c47f
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-11 22:29:17 +0000

    link to paramaterised meal-search route in Preview

--- src/components/MealPreview.tsx
+++ src/components/MealPreview.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Meal from "../utils/interfaces/IMeal";
 import IMealByIngredientOrNationOrCategory from "../utils/interfaces/IMealByIngredientOrNationOrCat";
 
@@ -8,9 +9,11 @@ interface IMealPreview {
 export default function MealPreviewA(props: IMealPreview): JSX.Element {
   const meal = props.meal;
   return (
-    <>
-      <h1>{meal.strMeal}</h1>
-      {meal.strMealThumb !== null && <img src={meal.strMealThumb} alt="" />}
-    </>
+    <Link to={`meal-search/${meal.strMeal}`}>
+      <div>
+        <h1>{meal.strMeal}</h1>
+        {meal.strMealThumb !== null && <img src={meal.strMealThumb} alt="" />}
+      </div>
+    </Link>
   );
 }
