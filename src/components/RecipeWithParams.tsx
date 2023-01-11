962ac8de039d97a25da07769173bdc0f34189020
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-11 23:46:07 +0000

    create RecipeWithParams

--- src/components/RecipeWithParams.tsx
+++ src/components/RecipeWithParams.tsx
@@ -1,18 +1,27 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Meal from "../utils/interfaces/IMeal";
-
-interface RecipeProps {
-  meal: Meal;
-}
+import Recipe from "./Recipe";
 
 export default function RecipeWithParams(): JSX.Element {
-  // const meal = props.meal;
-  const mealname = useParams();
-  return (
-    <>
-      <h1>{mealname}</h1>
-      {/* <h1>{meal.strMeal}</h1>
-      {meal.strMealThumb && <img src={meal.strMealThumb} alt="" />} */}
-    </>
-  );
+  const [meal, setMeal] = useState<Meal>();
+
+  const { id } = useParams();
+  const endpoint = "https://themealdb.com/api/json/v1/1/lookup.php?i=" + id;
+
+  useEffect(() => {
+    console.log(endpoint);
+    const fetchMealByID = async () => {
+      const response = await fetch(endpoint);
+      const meal: { meals: Meal[] } = await response.json();
+      setMeal(meal.meals[0]);
+    };
+    fetchMealByID();
+  }, [endpoint]);
+
+  if (meal) {
+    return <Recipe meal={meal} />;
+  } else {
+    return <></>;
+  }
 }
