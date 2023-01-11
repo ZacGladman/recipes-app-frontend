756b5f66bd1893069b826235980453ba947d12d9
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-11 23:46:48 +0000

    remove console log from createIngredientsArray Fn

--- src/utils/createIngredientsAndQuantsArray.ts
+++ src/utils/createIngredientsAndQuantsArray.ts
@@ -4,7 +4,6 @@ function filterMeal(meal: Meal, filterTerm: string) {
   const filteredKeyArray = Object.keys(meal).filter((key) =>
     key.includes(filterTerm)
   );
-  console.log(filteredKeyArray);
   const filteredMeal = filteredKeyArray.map((key) => meal[key]);
   const filteredMealNoEmptyStrings = filteredMeal.filter((string) => string);
   return filteredMealNoEmptyStrings;
