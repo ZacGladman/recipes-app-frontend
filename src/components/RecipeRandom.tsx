eaa889be451eb6399b99cf65567c531c1ef0f9dd
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-11 22:30:18 +0000

    create RecipeRandom

---
+++ src/components/RecipeRandom.tsx
@@ -0,0 +1,33 @@
+import { useCallback, useEffect, useState } from "react";
+import Meal from "../utils/interfaces/IMeal";
+import Recipe from "./Recipe";
+import { MdAutorenew } from "react-icons/md";
+
+interface RandomRecipeProps {
+  meal: Meal | null;
+}
+
+export default function RecipeRandom(props: RandomRecipeProps): JSX.Element {
+  const [meal, setMeal] = useState(props.meal);
+
+  const fetchRandomMeal = useCallback(async () => {
+    const response = await fetch(
+      "https://www.themealdb.com/api/json/v1/1/random.php"
+    );
+    const randomMeal: { meals: Meal[] } = await response.json();
+    setMeal(randomMeal.meals[0]);
+  }, [setMeal]);
+
+  useEffect(() => {
+    if (meal === null) {
+      fetchRandomMeal();
+    }
+  }, [meal, fetchRandomMeal]);
+
+  return (
+    <>
+      <MdAutorenew onClick={() => fetchRandomMeal()} />
+      <Recipe meal={meal} />
+    </>
+  );
+}
