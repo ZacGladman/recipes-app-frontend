fab64f2e010a1bb341116fbb8e4687ba381f4df6
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-11 23:45:43 +0000

    create ingredients section of Recipe

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,4 +1,5 @@
 import Meal from "../utils/interfaces/IMeal";
+import createIngredientsAndQuantsArray from "../utils/createIngredientsAndQuantsArray";
 
 interface RecipeProps {
   meal: Meal | null;
@@ -7,12 +8,21 @@ interface RecipeProps {
 export default function Recipe(props: RecipeProps): JSX.Element {
   const meal = props.meal;
   if (meal) {
+    const ingredientsAndQuantsArray = createIngredientsAndQuantsArray(meal);
     return (
       <>
         <h1>{meal.strMeal}</h1>
         {meal.strMealThumb && (
           <img src={meal.strMealThumb} alt="" className="recipe-image" />
         )}
+        {ingredientsAndQuantsArray.map((element) => {
+          return (
+            <p key={element.ingredient}>
+              {element.ingredient?.toLowerCase()}:{" "}
+              {element.quantity?.toLowerCase()}
+            </p>
+          );
+        })}
       </>
     );
   } else {
