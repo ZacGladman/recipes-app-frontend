de39097070c2d9a4d2d2066cffa657a358e5c325
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-11 22:32:09 +0000

    + createIngredientsAndQuantsArray, commented out

--- src/utils/createIngredientsAndQuantsArray.ts
+++ src/utils/createIngredientsAndQuantsArray.ts
@@ -9,3 +9,18 @@ function filterMeal(meal: Meal, filterTerm: string) {
   const filteredMealNoEmptyStrings = filteredMeal.filter((string) => string);
   return filteredMealNoEmptyStrings;
 }
+
+// export default function createIngredientsAndQuantsArray(
+//   meal: Meal
+// ): { ingredient: string; quantity: string }[] {
+//   const returnArray = [];
+//   const ingredientsArray = filterMeal(meal, "strIngredient");
+//   const quantitiesArray = filterMeal(meal, "strMeasure");
+//   for (let i = 0; i < ingredientsArray.length; i++) {
+//     returnArray.push({
+//       ingredient: ingredientsArray[i],
+//       quantity: quantitiesArray[i],
+//     });
+//   }
+//   return returnArray;
+// }
