478ccd87c1b728690871486681a9008dc0dcf5e5
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-12 22:52:28 +0000

    update Link path in MealPreviewA

--- src/components/MealPreviewA.tsx
+++ src/components/MealPreviewA.tsx
@@ -10,7 +10,7 @@ export default function MealPreviewA(props: IMealPreview): JSX.Element {
   const meal = props.meal;
   const id = meal.idMeal;
   return (
-    <Link to={`${id}`}>
+    <Link to={`../meal-search/${id}`}>
       <div>
         <h1>{meal.strMeal}</h1>
         {meal.strMealThumb !== null && <img src={meal.strMealThumb} alt="" />}
