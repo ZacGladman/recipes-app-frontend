94b9b10a75d50b05db8ecffec095b1e33fa434ce
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-12 22:53:01 +0000

    make OneCategory into a Link component

--- src/components/oneCategory.tsx
+++ src/components/oneCategory.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import ICategory from "../utils/interfaces/ICategory";
 
 interface IOneCategory {
@@ -7,11 +8,11 @@ interface IOneCategory {
 export default function OneCategory(props: IOneCategory): JSX.Element {
   const category = props.category;
   return (
-    <>
+    <Link to={`/meal-search/categories/${category.strCategory}`}>
       <h1>{category.strCategory}</h1>
       {category.strCategoryThumb && (
         <img src={category.strCategoryThumb} alt="category pic" />
       )}
-    </>
+    </Link>
   );
 }
