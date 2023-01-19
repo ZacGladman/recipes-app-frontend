2f6c9edbd6cddb74124301e42f9ccc20173373ac
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-19 22:37:23 +0000

    declare baseURL in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
+export const baseURL =
+  process.env.NODE_ENV === "production"
+    ? "https://chefbookdb.onrender.com"
+    : "http://localhost:4000";
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
