6ad407f3b4ab65f318ea4f20d4de5bdb90db9fff
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-31 21:44:41 +0000

    write initial Reviews page

--- src/Pages/Reviews.tsx
+++ src/Pages/Reviews.tsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+import { baseURL } from "..";
+import IReviewFromDB from "../utils/interfaces/IReviewFromDB";
+
 export default function Reviews(): JSX.Element {
+  const [tenNewestReviews, setTenNewestReviews] = useState<
+    IReviewFromDB[] | null
+  >(null);
+  useEffect(() => {
+    const fetch10NewestReviews = async () => {
+      const response = await fetch(baseURL + "/reviews/newest-10");
+      const newestReviews = await response.json();
+      setTenNewestReviews(newestReviews);
+    };
+    fetch10NewestReviews();
+  }, []);
+
   return <h1>reviews</h1>;
 }
